0f28f4a44cb399ede94c21b09648724246bc37d6
phamtrongnghia <nhoxqpn369@gmail.com>
2017-11-07 14:20:10 +0700

    test loi

--- src/tdddemo/Rating.java
+++ src/tdddemo/Rating.java
@@ -11,12 +11,12 @@ package tdddemo;
  */
 public class Rating {
     public static String Rating(double gpa) {
-        if (gpa > 10) return "Invalid Grade";
-        else if (gpa >= 9) return "Excellence";
-        else if (gpa >= 8) return "Good";
-        else if (gpa >= 7) return "Faird";
-        else if (gpa >= 5) return "Average";
-        else if (gpa >= 0) return "Tre trau!";
-        else return "Invalid Grade";
+//        if (gpa > 10) return "Invalid Grade";
+//        else if (gpa >= 9) return "Excellence";
+//        else if (gpa >= 8) return "Good";
+//        else if (gpa >= 7) return "Faird";
+//        else if (gpa >= 5) return "Average";
+//        else if (gpa >= 0) return "Tre trau!";
+//        else return "Invalid Grade";
     }
 }
